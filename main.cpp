053ef18279f2ce8a63249373b84b9149f3904813
OxSami <nouhasssami23@gmail.com>
2024-03-25 23:34:07 +0000

    using namespace std;
    int main(){
        long long n;
        cin >> n;
    
        for(int x = 1 ; x <= n ; x++){
            for(int y = 1 ; y <= n ; y++){
                long long X = pow(x,2);
                long long Y = pow(y,2);
                if(static_cast<int>(X + Y) % n == 0){
                    cout << x << " " << y << endl;
                    return 0;
                } else {
                    continue;
                }
            }
        }
        cout << "No solutions";
        return 0;
    }

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    double n = 14 / 12;
-    cout << n;
+    long long n;
+    cin >> n;
 
+    for(int x = 0 ; x <= n ; x++){
+        for(int y = 0 ; y <= n ; y++){
+            long long X = pow(x,2);
+            long long Y = pow(y,2);
+            if(static_cast<int>(X + Y) % n == 0){
+                cout << x << " " << y << endl;
+                return 0;
+            } else {
+                continue;
+            }
+        }
+    }
+    cout << "No solutions";
     return 0;
 }
